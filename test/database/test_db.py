ccb8985918be7f412b9d2a15045cd63a3c7a4ae2
Fr4nt1x <Fr4nt1x@Muc.de>
2021-04-21 16:21:05 +0200

    Fix lint warnings

--- test/database/test_db.py
+++ test/database/test_db.py
@@ -1,6 +1,7 @@
 from database.db import DB
 import os
-import datetime, time
+import datetime
+
 
 class TestDB:
     data_base_path = ()
@@ -9,9 +10,9 @@ class TestDB:
         path = os.path.join(tmpdir, "db.json")
         db = DB(path)
         last_meal_date = datetime.date.today().isoformat()
-        meal1 = {"name": "meal1 bla bla", "difficulty":3, "count":20, "last_date": last_meal_date}
+        meal1 = {"name": "meal1 bla bla", "difficulty": 3, "count": 20, "last_date": last_meal_date}
         db.add_meal(meal1)
-        last_meal_date = datetime.date(2021,10,12).isoformat()
+        last_meal_date = datetime.date(2021, 10, 12).isoformat()
         meal2 = {"name": "meal2-blü_blÖ", "difficulty": 2, "count": 1, "last_date": last_meal_date}
         db.add_meal(meal2)
         allMeals = db.get_all_meals()
