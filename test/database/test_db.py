75fa096e620c4e502cdb639e79c02118bdfdff1c
Fr4nt1x <Fr4nt1x@Muc.de>
2021-04-21 16:07:23 +0200

    Add assert to test

--- test/database/test_db.py
+++ test/database/test_db.py
@@ -7,11 +7,12 @@ class TestDB:
 
     def test_add_meal(self, tmpdir):
         path = os.path.join(tmpdir, "db.json")
-        db = DB("test.json")
+        db = DB(path)
         last_meal_date = datetime.date.today().isoformat()
         meal1 = {"name": "meal1 bla bla", "difficulty":3, "count":20, "last_date": last_meal_date}
         db.add_meal(meal1)
         last_meal_date = datetime.date(2021,10,12).isoformat()
         meal2 = {"name": "meal2-blü_blÖ", "difficulty": 2, "count": 1, "last_date": last_meal_date}
         db.add_meal(meal2)
-        print(db.get_all_meals())
+        allMeals = db.get_all_meals()
+        assert [meal1, meal2] == allMeals
