a3da205aead1eda9351353ea7ada9abbc40ee4c6
Fr4nt1x <Fr4nt1x@Muc.de>
2021-04-28 16:22:38 +0200

    Add Cors for axios response

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database.db import DB
 from schemas.meal import Meal
 
 app = FastAPI()
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 db = DB('test.json')
 
 
