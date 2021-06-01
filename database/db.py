0a8a2127cd93c28248e3de6b6e4ec6f55b39c5ae
Fr4nt1x <Fr4nt1x@Muc.de>
2021-06-01 13:38:54 +0200

    Add test for tags. BIg db added

--- database/db.py
+++ database/db.py
@@ -1,4 +1,4 @@
-from tinydb import TinyDB
+from tinydb import TinyDB, Query
 from tinydb.middlewares import CachingMiddleware
 from tinydb.storages import JSONStorage
 
@@ -11,10 +11,17 @@ class DB(object):
 
     def __init__(self, path):
         TinyDB.default_table_name = self.MEAL_TABLE_NAME
-        self.db = TinyDB(path, storage=CachingMiddleware(JSONStorage))
+        self.db = TinyDB(path, storage=CachingMiddleware(JSONStorage), indent=4)
 
     def add_meal(self, meal: Meal):
         self.db.insert(meal.as_dict())
 
     def get_all_meals(self):
         return self.db.all()
+
+    def change_tags_for_meal(self, meal: Meal):
+        meal_query = Query()
+        self.db.update({"tags": meal.tags}, meal_query.name == meal.name)
+
+    def close(self):
+        self.db.close()
