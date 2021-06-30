17e0c19641dc433e0dc234929d5a0920082591e2
fr4nt1x <fr4nt1x@users.noreply.github.com>
2021-06-30 15:15:57 +0200

    Dates (#11)
    * Add multiple dates and add date for meal
    
    * Add update date for meals
    
    Co-authored-by: Fr4nt1x <Fr4nt1x@Muc.de>

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Run on localhost:
 uvicorn main:app --reload
+
+Run in network:
+uvicorn main:app --host 0.0.0.0 --port 8000
--- schemas/meal.py
+++ schemas/meal.py
@@ -8,13 +8,13 @@ class Meal(BaseModel):
     name: str
     tags: List[str]
     count: int
-    last_date: datetime.date
+    last_dates: List[datetime.date]
 
     @staticmethod
     def from_dict(input_dict):
         return Meal(name=input_dict["name"], tags=input_dict["tags"], count=input_dict["count"],
-                    last_date=input_dict["last_date"])
+                    last_dates=input_dict["last_dates"])
 
     def as_dict(self):
         return {'name': self.name, 'tags': self.tags, 'count': self.count,
-                'last_date': self.last_date.isoformat()}
+                'last_dates': [d.isoformat() for d in sorted(self.last_dates)]}
