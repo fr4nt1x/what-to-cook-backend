671e3512c9a6286b0a4d9a0d627db6dae34b1b80
Fr4nt1x <Fr4nt1x@Muc.de>
2021-07-21 16:39:08 +0200

    Add remove date from meal api call

--- database/db.py
+++ database/db.py
@@ -15,11 +15,26 @@ class DB(object):
 
     def add_meal(self, meal: Meal):
         self.db.insert(meal.as_dict())
+        return True
+
+    def remove_date_from_meal(self, meal: Meal, date: str):
+        meal_query = Query()
+        found_meal = self.db.search(meal_query.name == meal.name)
+        if len(found_meal) == 1:
+            found_meal = found_meal[0]
+            if date in found_meal['last_dates']:
+                found_meal['last_dates'].remove(date)
+                self.db.update(
+                    {"last_dates": sorted(found_meal['last_dates'])},
+                    meal_query.name == meal.name)
+                return True
+        return False
 
     def update_dates_for_meal(self, meal: Meal):
         meal_query = Query()
         self.db.update({"last_dates": list(set([d.isoformat() for d in sorted(meal.last_dates)]))},
                        meal_query.name == meal.name)
+        return True
 
     def get_all_meals(self):
         return self.db.all()
@@ -27,6 +42,7 @@ class DB(object):
     def change_tags_for_meal(self, meal: Meal):
         meal_query = Query()
         self.db.update({"tags": meal.tags}, meal_query.name == meal.name)
+        return True
 
     def close(self):
         print("close")
